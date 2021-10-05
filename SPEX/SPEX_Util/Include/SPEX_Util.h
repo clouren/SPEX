eefaece755f43d2b392659e412028b643e564924
Lorena <lorenamejiadomenzain@outlook.com>
2021-10-04 21:27:10 -0500

    last changes before big pull

--- SPEX/SPEX_Util/Include/SPEX_Util.h
+++ SPEX/SPEX_Util/Include/SPEX_Util.h
@@ -918,5 +918,11 @@ SPEX_info SPEX_determine_symmetry
             // the values
 );
 
+SPEX_info SPEX_determine_empty_column
+(
+    bool empty_column_exists, //true if A has a column of only 0s
+    SPEX_matrix* A
+);
+
 #endif
 
