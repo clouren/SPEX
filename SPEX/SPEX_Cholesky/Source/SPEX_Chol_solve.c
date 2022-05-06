fa90d7058f315d64b1a6e970963b8047fd10ba87
Lorena <lorenamejiadomenzain@outlook.com>
2022-05-06 11:12:01 -0500

    issues with solve

--- SPEX/SPEX_Cholesky/Source/SPEX_Chol_solve.c
+++ SPEX/SPEX_Cholesky/Source/SPEX_Chol_solve.c
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// SPEX_Chol/SPEX_Chol_Solve: Solves the system LDL' x = b.
+// SPEX_Chol/SPEX_Chol_Solve: Solve the SPD linear system after factorization
 //------------------------------------------------------------------------------
 
 // SPEX_Cholesky: (c) 2021, Chris Lourenco, United States Naval Academy, 
@@ -12,7 +12,6 @@
 #define SPEX_FREE_WORKSPACE        \
 {                                  \
     SPEX_matrix_free(&b2, option); \
-    SPEX_matrix_free(&x2, option);  \
 }
 
 # define SPEX_FREE_ALL             \
@@ -20,163 +19,169 @@
     SPEX_FREE_WORKSPACE            \
     SPEX_matrix_free(&x, NULL);    \
 }
-//TODO have a separate "clusterVersion" for all the prints, so they are here
+//TODO maybe have a separate "clusterVersion" for all the prints, so they are def not in the release version
 #include "spex_chol_internal.h"
-    
+
 /* Purpose: This function solves the linear system LDL' x = b.
  *
  * Input arguments:
  * 
- * x_handle:        A handle to the solution matrix. NULL on input.
- *                  On output contains a pointer to the solution vector(s).
+ * x_handle:        A handle to the solution matrix. On input this is NULL,
+ *                  on output x_handle contains a pointer to the solution
+ *                  vector(s)
  * 
- * F:               Factorization struct. Contains the REF Cholesky factorization
- *                  of A along with permutation and elimination tree information.
+ * PAP:             Permuted version of the input matrix
  * 
- * b:               Right hand side vector(s). Must be SPEX_MPZ and SPEX_DENSE.
+ * A:               Nonpermuted (original) input matrix
  * 
- * option:          Command options. Default if NULL.
+ * b:               Right hand side vector(s)
+ * 
+ * rhos:            Sequence of factorization pivots
+ * 
+ * L:               Lower triangular matrix
+ * 
+ * S:               Symbolic analysis struct (contains row/column permutation
+ *                  and its inverse
+ * 
+ * option:          Command options
+ * 
  */
 
-SPEX_info SPEX_Chol_solve       // Solves the linear system LDL' x = b
+SPEX_info SPEX_Chol_solve
 (
     // Output
-    SPEX_matrix** x_handle,           // On input: NULL.
-                                      // On output: Exact rational solution (SPEX_MPQ)
+    SPEX_matrix** x_handle,           // On input: undefined.
+                                      // On output: Rational solution (SPEX_MPQ)
                                       // to the system. 
-    // Input
-    const SPEX_factorization* F,      // Cholesky factorization. Must contain the
-                                      // elimination tree of A,  
-                                      // column pointers of L, exact number of
-                                      // nonzeros of L and permutation used //TOCHECK
-    const SPEX_matrix* b,             // Right hand side vector(s).
-                                      // Must be SPEX_MPZ and SPEX_DENSE.
-    const SPEX_options* option        // Command options. Default if NULL.
+    // input/output:
+    SPEX_factorization *F,  // The non-updatable Cholesky factorization.
+                            // Mathematically, F is unchanged.  However, if F
+                            // is updatable on input, it is converted to
+                            // non-updatable.  If F is already non-updatable,
+                            // it is not modified.
+    // input:
+    const SPEX_matrix* b,             // Right hand side vector
+    const SPEX_options* option        // command options
 )
 {
     SPEX_info info;
 
-    if (!spex_initialized()) return SPEX_PANIC; //TODO use this one in the other functions
+    if (!spex_initialized()) return SPEX_PANIC;
   
     // Check the inputs
-    ASSERT(F->kind==SPEX_CHOLESKY_FACTORIZATION); //TODO make part of if!
+    ASSERT(!x_handle);
+    ASSERT(b->type == SPEX_MPZ);
+    ASSERT(b->kind == SPEX_DENSE);
+    ASSERT(F->kind==SPEX_CHOLESKY_FACTORIZATION);
 
     if (!x_handle || b->type != SPEX_MPZ || b->kind != SPEX_DENSE)
     {
         return SPEX_INCORRECT_INPUT;
     }
     
-  
-    // Indices, m, n and nz
-    int64_t i, j, m = b->m, n = b->n, nz;
-    // Set the number of nonzeros in b
-    nz = m * n;
-  
-    // det is the determinant of the PAP matrix. It is obtained for free
-    // from the SPEX Cholesky factorization det = rhos[L->n-1] = L[L->n,L->n]
-    mpz_t* det = NULL;
-    det = &(F->rhos->x.mpz[F->L->n-1]);
-    size_t bitsDet = mpz_sizeinbase((*det),2); //CLUSTER
-    //gmp_printf("%llu, ", (bitsDet)); //CLUSTER
+    // convert the factorization F to non-updatable
+    info = SPEX_factorization_convert(F, false, option);
+    if (info != SPEX_OK) return info;
+    
+    //int64_t i, j, nz, n = F->L->n;
 
+    // det is the determinant of the PAP matrix. It is obtained for free
+    // from the SPEX Cholesky factorization det = rhos[n-1] = L[n,n]
+    mpz_t* det = NULL;
+
+    //--------------------------------------------------------------------------
     // Declare workspace and output
-    // x2 is the (permuted) final solution vector
-    SPEX_matrix *x2 = NULL;
-    // x is the final solution vector returned to user
+    //--------------------------------------------------------------------------
+    // x is the permuted final solution vector returned to the user
     SPEX_matrix *x = NULL;
     // b2 is the permuted right hand side vector(s)
     SPEX_matrix *b2 = NULL;
 
-    // Allocate memory for b2
-    SPEX_CHECK( SPEX_matrix_allocate(&b2, SPEX_DENSE, SPEX_MPZ, m, n, 
-                                     nz, false, true, option) );
+    //--------------------------------------------------------------------------
+    // get b2 = Pinv*b
+    //--------------------------------------------------------------------------
 
-    // Set b2[i,j] = b[pinv[i],j] to permute the RHS vectors. We
-    // will then solve PAP b2 = b2 overwriting these permuted vectors
-    // with the scaled integer solution
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)   
-        {
-            SPEX_CHECK( SPEX_mpz_set(SPEX_2D(b2, F->Pinv_perm[i], j, mpz),
-                                     SPEX_2D(b, i, j, mpz)) );
-        }
-    }
+    SPEX_CHECK (spex_permute_dense_matrix (&b2, b, F->Pinv_perm, option)) ;
 
+    //--------------------------------------------------------------------------
     // Forward substitution, b2 = L \ b2. Note that b2 is overwritten    
-    SPEX_CHECK( spex_chol_forward_sub(b2, F->L, F->rhos) );
+    //--------------------------------------------------------------------------
 
-    // Apply the determinant to b2: b2 = b2*det(A)
-    for (i = 0; i < nz; i++)
-    {
-        SPEX_CHECK( SPEX_mpz_mul(b2->x.mpz[i], b2->x.mpz[i], (*det)) );
-    }
+    SPEX_CHECK(spex_chol_forward_sub(b2, F->L, F->rhos));
+
+    //--------------------------------------------------------------------------
+    // Apply the determinant to b2, b2 = det*b2
+    //--------------------------------------------------------------------------
+
+    // Set the value of the determinant det = rhos[n-1] 
+    det = &(F->rhos->x.mpz[F->L->n-1]);
+    // FIXME unused variable (commented by Jinhao)
+    // size_t bitsDet = mpz_sizeinbase((*det),2); //CLUSTER
+    //gmp_printf("%llu, ", (bitsDet)); //CLUSTER
     
+    SPEX_CHECK(spex_matrix_mul(b2, (*det) ));
+    
+    //--------------------------------------------------------------------------
     // Backsolve, b2 = L' \ b2. Note that, again, b2 is overwritten
-    SPEX_CHECK( spex_chol_backward_sub(b2, F->L) );
+    //--------------------------------------------------------------------------
 
-    // Allocate memory for the (permuted) solution vector x2 = b2/det
-    SPEX_CHECK( SPEX_matrix_allocate(&x2, SPEX_DENSE, SPEX_MPQ, m, n,
-                                     0, false, true, option) ) ;
-/* TODO DELETE ME
+    SPEX_CHECK(spex_chol_backward_sub(b2, F->L));
+
+    //--------------------------------------------------------------------------
+    // get real solution x by applying both permutation and scale
+    // x = P*b2/scale
+    //--------------------------------------------------------------------------    // Scale is the scaling factor for the solution vectors.
+    // When the forward/backsolve is complete, the entries in
+    // x/det are rational, but are solving the scaled linear system
+    // A' x = b' (that is if A had input which was rational or floating point
+    // and had to be converted to integers). Thus, the scale here is used
+    // to convert x into into the actual solution of A x = b. 
+    // Mathematically, set scale = b->scale * rhos[n-1] / PAP->scale
+    SPEX_CHECK(SPEX_mpq_set_z(b2->scale, (*det)));
+    SPEX_CHECK(SPEX_mpq_mul(b2->scale, b2->scale, b->scale));
+    SPEX_CHECK(SPEX_mpq_div(b2->scale, b2->scale, F->scale_for_A));
+
+    // allocate space for x as dense MPQ matrix
+    SPEX_CHECK (SPEX_matrix_allocate (&x, SPEX_DENSE, SPEX_MPQ, b->m, b->n,
+        0, false, true, option));
+/*
     size_t bitsSmall=mpz_sizeinbase(b2->x.mpz[0],2); //CLUSTER
     size_t bitsLarge=0; //CLUSTER
     size_t bitsTemp; //CLUSTER
     int64_t bitsSum=0; //CLUSTER
-    // Set x2[i] = b2[i]/det
+    // Set x[i] = b2[i]/det
 */
-    // Compute the solution vector to the system PAP x2 = b2
-    // This is done by setting x2 = b2/det
-    for (i = 0; i < nz; i++)
+
+    // obtain x from permuted b2 with scale applied
+    for (int64_t i = 0 ; i < b->m ; i++)
     {
-        /* TODO DELETE ME
-        bitsTemp=mpz_sizeinbase(b2->x.mpz[i],2); //CLUSTER
-        if(bitsTemp>bitsLarge) //CLUSTER
-        { 
-            bitsLarge=bitsTemp; //CLUSTER
-        }
-        else if(bitsTemp<bitsSmall) //CLUSTER
+        int64_t pi = F->P_perm[i];
+        for (int64_t j = 0 ; j < b->n ; j++)
         {
-                bitsSmall=bitsTemp; //CLUSTER
-        }
-        bitsSum=bitsSum+(int64_t)bitsTemp; //CLUSTER
+            /*
+            bitsTemp=mpz_sizeinbase(SPEX_2D(b2,  i, j, mpz),2); //CLUSTER
+            if(bitsTemp>bitsLarge) //CLUSTER
+            { 
+                bitsLarge=bitsTemp; //CLUSTER
+            }
+            else if(bitsTemp<bitsSmall) //CLUSTER
+            {
+                    bitsSmall=bitsTemp; //CLUSTER
+            }
+            bitsSum=bitsSum+(int64_t)bitsTemp; //CLUSTER
 */
-        // Set the numerator of x2[i] = b2[i]
-        SPEX_CHECK( SPEX_mpq_set_num(x2->x.mpq[i], b2->x.mpz[i]) );
-        // Set the denominator of x2[i] = det
-        SPEX_CHECK( SPEX_mpq_set_den(x2->x.mpq[i], (*det)) );
-        // Remove common factors from numerator and denominator
-        SPEX_CHECK( SPEX_mpq_canonicalize(x2->x.mpq[i]) );
+            SPEX_CHECK(SPEX_mpq_set_z(SPEX_2D(x,  pi, j, mpq),
+                                      SPEX_2D(b2,  i, j, mpz)));
+            SPEX_CHECK(SPEX_mpq_div(SPEX_2D(x,  pi, j, mpq),
+                                    SPEX_2D(x,  pi, j, mpq), b2->scale));
+        }
     }
-    /* TODO DELETE ME
+
+    /*
     gmp_printf("%llu, %llu, %llu, ", bitsSmall, bitsLarge, bitsSum); //CLUSTER
     */
-    // Allocate memory for x which is the final solution vector of the 
-    // original (unpermuted) linear system. 
-    SPEX_CHECK( SPEX_matrix_allocate(&x, SPEX_DENSE, SPEX_MPQ, m, n, 0, false, true, option) );
-    
-    // Undo the inverse row permutation on x2. This is done by applying the row permutation
-    // to the indices of x. That is x[p[i], j] = x2[i,j]
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            SPEX_CHECK( SPEX_mpq_set(SPEX_2D(x, F->P_perm[i], j, mpq), 
-                                     SPEX_2D(x2, i, j, mpq)) );
-        }
-    }
 
-    //--------------------------------------------------------------------------
-    // Scale the solution if necessary.
-    //--------------------------------------------------------------------------
-    // When the forward/backsolve is complete, the entries in
-    // x are rational, but are solving the scaled linear system
-    // A' x = b' (that is if A had input which was rational or floating point
-    // and had to be converted to integers). Thus, the scale here is used
-    // to convert x into into the actual solution of A x = b. 
-    SPEX_CHECK( SPEX_scale(x, F->scale_for_A, b->scale, option) );
-
-    // Set output, free memory, return success
+    // Set output, free memory
     (*x_handle) = x;
     SPEX_FREE_WORKSPACE;
     return SPEX_OK;
