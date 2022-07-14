f0068a13113f4453280396266ee093be47c0170c
LoreMD <lorena.m.d@isen-3sw4pf3.engr.tamu.edu>
2022-07-14 07:42:38 -0500

    python backslash

---
+++ SPEX/Python/SPEX/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SPEX',
+    packages=find_packages(include=['SPEX_Choleskpy']),
+    version='0.1.0',
+    description='Python interface for SPEX',
+    author='Lorena Mejia Domenzain',
+    license='GNU',
+)
+
+#TODO update
