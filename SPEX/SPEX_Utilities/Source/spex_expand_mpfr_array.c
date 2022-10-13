1ef7db268d868f0a51fe9be66df531e839fc3906
Lorena <lorenamejiadomenzain@outlook.com>
2022-10-13 09:12:35 -0500

    last TODOs

--- SPEX/SPEX_Utilities/Source/spex_expand_mpfr_array.c
+++ SPEX/SPEX_Utilities/Source/spex_expand_mpfr_array.c
@@ -15,8 +15,6 @@
  */
 
 #define SPEX_FREE_ALL               \
-    SPEX_MPFR_CLEAR(expon);         \
-    SPEX_MPZ_CLEAR(temp_expon);     \
     SPEX_MPZ_CLEAR(gcd);            \
     SPEX_MPZ_CLEAR(one);            \
     SPEX_MPQ_CLEAR(temp);           \
@@ -48,42 +46,21 @@ SPEX_info spex_expand_mpfr_array
     int64_t i, k ;
     int r1, r2 = 1 ;
     bool nz_found = false;
-    mpfr_t expon; SPEX_MPFR_SET_NULL(expon);
-    mpz_t temp_expon, gcd, one;
+    mpz_t gcd, one;
     SPEX_matrix* x3 = NULL;
-    SPEX_MPZ_SET_NULL(temp_expon);
     SPEX_MPZ_SET_NULL(gcd);
     SPEX_MPZ_SET_NULL(one);
     mpq_t temp; SPEX_MPQ_SET_NULL(temp);
 
-    uint64_t prec = SPEX_OPTION_PREC (option) ;
-    mpfr_rnd_t round = SPEX_OPTION_ROUND (option) ;
-
     SPEX_CHECK(SPEX_mpq_init(temp));
-    SPEX_CHECK(SPEX_mpfr_init2(expon, prec));
-    SPEX_CHECK(SPEX_mpz_init(temp_expon));
     SPEX_CHECK(SPEX_mpz_init(gcd));
     SPEX_CHECK(SPEX_mpz_init(one));
 
     SPEX_CHECK (SPEX_matrix_allocate(&x3, SPEX_DENSE, SPEX_MPFR, n, 1, n,
         false, true, option));
 
-    // TODO: Discuss: Should we not just cast MPFR->rational->mpz?
-    // This gets rid of doing the 2^prec crap
-    // Erick/Tim what do you think?
-    // expon = 2^prec (overestimate)
-    SPEX_CHECK(SPEX_mpfr_ui_pow_ui(expon, 2, prec, round)) ;
-
-    for (i = 0; i < n; i++)
-    {
-        // x3[i] = x[i]*2^prec
-        SPEX_CHECK(SPEX_mpfr_mul(x3->x.mpfr[i], x[i], expon, round));
-
-        // x_out[i] = x3[i]
-        SPEX_CHECK(SPEX_mpfr_get_z(x_out[i], x3->x.mpfr[i], round));
-    }
-    SPEX_CHECK(SPEX_mpfr_get_z(temp_expon, expon, round));
-    SPEX_CHECK(SPEX_mpq_set_z(scale, temp_expon));
+    SPEX_CHECK(spex_cast_array(x3,SPEX_MPQ,x,SPEX_MPFR,n,NULL,NULL,option));
+    SPEX_CHECK(spex_cast_array(x_out,SPEX_MPZ,x3,SPEX_MPQ,n,scale,NULL,option));
 
     //--------------------------------------------------------------------------
     // Find the gcd to reduce scale
