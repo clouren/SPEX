76ff64c3029d03b7ef3ca9e90b2cbdaad68b61a3
Erick <emc@tamu.edu>
2022-11-23 11:57:13 -0600

    Update SPEX_free.c
    TEST of Git in Windows

--- SPEX/SPEX_Utilities/Source/SPEX_free.c
+++ SPEX/SPEX_Utilities/Source/SPEX_free.c
@@ -2,6 +2,8 @@
 // SPEX_Utilities/SPEX_free: wrapper for free
 //------------------------------------------------------------------------------
 
+//TEST 
+
 // SPEX_Utilities: (c) 2019-2021, Chris Lourenco, Jinhao Chen,
 // Erick Moreno-Centeno, Timothy A. Davis, Texas A&M.  All Rights Reserved.
 // SPDX-License-Identifier: GPL-2.0-or-later or LGPL-3.0-or-later
