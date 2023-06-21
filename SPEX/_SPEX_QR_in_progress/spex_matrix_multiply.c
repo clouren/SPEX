5220cf36288081400eb67b214b6ff15fe4165c79
LoreMD <lorena.m.d@isen-3sw4pf3.engr.tamu.edu>
2023-06-21 09:07:44 -0500

    modifying csparse to fit spex for mat mult

--- SPEX/_SPEX_QR_in_progress/spex_matrix_multiply.c
+++ SPEX/_SPEX_QR_in_progress/spex_matrix_multiply.c
@@ -20,36 +20,47 @@ SPEX_info spex_matrix_multiply
 )
 {
     //Declare variables
-    csi p, j, nz = 0, anz, *Cp, *Ci, *Bp, m, n, bnz, *w, values, *Bi ;
-    double *x, *Bx, *Cx ;
+    int64_t p, j, nz = 0, anz, *Cp, *Ci, *Bp, m, n, bnz, *w, values, *Bi ;
+    SPEX_matrix *x, *Bx, *Cx ;
     //Allocate memory for C
-    cs *C ;
+    SPEX_matrix *C;
 
     //Check inputs
-    if (!CS_CSC (A) || !CS_CSC (B)) return (NULL) ;    
+    //if (!CS_CSC (A) || !CS_CSC (B)) return (NULL) ;    
     if (A->n != B->m) return (NULL) ;
 
 
     m = A->m ; anz = A->p [A->n] ;
     n = B->n ; bnz = B->p [n] ;
-    w = cs_calloc (m, sizeof (csi)) ;                    /* get workspace */
+    w = cs_calloc (m, sizeof (int64_t)) ;                    /* get workspace */
     values = (A->x.mpz != NULL) && (B->x.mpz != NULL) ;
-    x = values ? cs_malloc (m, sizeof (double)) : NULL ; /* get workspace */
-    C = cs_spalloc (m, n, anz + bnz, values, 0) ;        /* allocate result */
-    if (!C || !w || (values && !x)) return (cs_done (C, w, x, 0)) ;
+    //x = values ? cs_malloc (m, sizeof (double)) : NULL ; /* get workspace */
+    if(values)
+    {
+        SPEX_CHECK(SPEX_matrix_allocate(&x, SPEX_DENSE, SPEX_MPZ, m, 1, m,
+        false, true, NULL));
+    }
+    else
+    {
+        return SPEX_OUT_OF_MEMORY;
+    }
+    
+    SPEX_CHECK(SPEX_matrix_allocate(&C, SPEX_CSC, SPEX_MPZ, m, n, anz+bnz,
+        false, true, NULL));
+    if (!C || !w || (values && !x)) return (SPEX_OUT_OF_MEMORY) ;
     
     for (j = 0 ; j < n ; j++)
     {
         if (nz + m > C->nzmax && !cs_sprealloc (C, 2*(C->nzmax)+m))
         {
-            return (cs_done (C, w, x, 0)) ;             /* out of memory */
+            return (SPEX_OUT_OF_MEMORY) ;             /* out of memory */
         } 
         C->p [j] = nz ;                   /* column j of C starts here */
         for (p = B->p [j] ; p < B->p [j+1] ; p++)
         {
-            nz = spex_scatter (A, B->i [p], B->x ? B->x [p] : 1, w, x, j+1, C, nz) ;
+            spex_scatter (A, B->i [p], B->x.mpz ? B->x.mpz [p] : 1, w, x, j+1, C, nz) ;//returns nz
         }
-        if (values) for (p = C->p [j] ; p < nz ; p++) C->x [p] = x [C->i [p]] ;
+        if (values) for (p = C->p [j] ; p < nz ; p++) C->x.mpz [p] = x->x.mpz [C->i [p]] ;
     }
     C->p [n] = nz ;                       /* finalize the last column of C */
 
--- SPEX/_SPEX_QR_in_progress/spex_scatter.c
+++ SPEX/_SPEX_QR_in_progress/spex_scatter.c
@@ -1,1 +1,28 @@
-//TODO
+// CSparse/Source/cs_scatter: scatter a scaled sparse vector into a dense vector
+// CSparse, Copyright (c) 2006-2022, Timothy A. Davis. All Rights Reserved.
+// SPDX-License-Identifier: LGPL-2.1+
+#include "cs.h"
+/* x = x + beta * A(:,j), where x is a dense vector and A(:,j) is sparse */
+SPEX_info spex_scatter (const SPEX_matrix *A, int64_t j, mpz_t beta, int64_t *w, SPEX_matrix *x, int64_t mark,
+  SPEX_matrix *C, int64_t nz)
+{
+    int64_t i, p, *Ap, *Ai, *Ci ;
+    SPEX_REQUIRE (A, SPEX_CSC,   SPEX_MPZ);
+    SPEX_REQUIRE (A, SPEX_CSC,   SPEX_MPZ);
+    if (!w ) return (SPEX_INCORRECT_INPUT) ;     /* check inputs */
+        
+    Ap = A->p ; Ai = A->i ; ; Ci = C->i ;
+    
+    for (p = Ap [j] ; p < Ap [j+1] ; p++)
+    {
+        i = Ai [p] ;                            /* A(i,j) is nonzero */
+        if (w [i] < mark)
+        {
+            w [i] = mark ;                      /* i is new entry in column j */
+            Ci [nz++] = i ;                     /* add i to pattern of C(:,j) */
+            if (x) SPEX_MPZ_MUL(x [i], beta, A->x.mpz [p]) ;      /* x(i) = beta*A(i,j) */
+        }
+        else if (x) SPEX_MPZ_ADDMUL(x [i], beta , A->x.mpz [p]) ;    /* i exists in C(:,j) already x [i] += beta * Ax [p] */
+    }
+    return (nz) ;
+}
