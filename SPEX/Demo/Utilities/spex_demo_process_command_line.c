500f43fa2ba2617d028fde8905578e6653f1195e
clouren <chrisjlourenco@gmail.com>
2024-06-10 11:03:26 -0400

    Fix Demo utility

--- SPEX/Demo/Utilities/spex_demo_process_command_line.c
+++ SPEX/Demo/Utilities/spex_demo_process_command_line.c
@@ -37,12 +37,12 @@ SPEX_info spex_demo_process_command_line //processes the command line
         {
             if (!argv[++i])
             {
-                printf("\n****ERROR! There must be an argument between 0-2"
-                    "following q\n");
+                printf("\n****ERROR! There must be an argument between 1-3"
+                    "following q\n 1: none, 2: Colamd, 3: AMD");
                 return SPEX_INCORRECT_INPUT;
             }
             option->order = atoi(argv[i]);
-            if (option->order < 0 || option->order > 2)
+            if (option->order < 1 || option->order > 3)
             {
                 printf("\n****ERROR! Invalid column ordering"
                     "\nDefaulting to COLAMD\n\n");
