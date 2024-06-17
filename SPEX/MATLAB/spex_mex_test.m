cacf5c49bd2feff31c2cebd862e11b00445f2683
Tim Davis <davis@tamu.edu>
2024-06-17 11:13:31 -0500

    test LDL backslash in spex_test_test.m

--- SPEX/MATLAB/spex_mex_test.m
+++ SPEX/MATLAB/spex_mex_test.m
@@ -6,8 +6,6 @@ function spex_mex_test
 % See also spex_backslash, spex_lu_backslash, spex_cholesky_backslash,
 %   spex_ldl_backslash, spex_mex_install, spex_mex_demo.
 
-% FIXME NOW: does not test spex_ldl_backslash
-
 % SPEX: (c) 2022-2024, Christopher Lourenco, Jinhao Chen,
 % Lorena Mejia Domenzain, Erick Moreno-Centeno, and Timothy A. Davis.
 % All Rights Reserved.
@@ -86,12 +84,12 @@ end
 fprintf ('\nmaxerr: %g\n', maxerr) ;
 
 if (maxerr < 1e-6)
-    fprintf('\nSPEX LU installation successful\n')
+    fprintf('\nSPEX LU tests successful\n')
 else
-    error ('\nTesting failure!  error too high please reinstall\n')
+    error ('\nTesting failure!  error too high\n')
 end
 
-fprintf ('Testing SPEX Cholesky: ') ;
+fprintf ('Testing SPEX Cholesky and LDL: ') ;
 
 % First, check if we can use a real life sparse matrix via ssget
 if (exist ('ssget') ~= 0)
@@ -103,8 +101,11 @@ if (exist ('ssget') ~= 0)
     b = rand(m, 1);
     fprintf ('.') ;
     x = spex_cholesky_backslash(A,b);
+    x1 = spex_ldl_backslash(A,b);
     x2 = A\b;
     err = norm(x-x2)/norm(x);
+    maxerr = max (maxerr, err) ;
+    err = norm(x-x1)/norm(x);
     maxerr = max (maxerr, err) ;
 
     % now convert to an integer problem (x will not be integer)
@@ -112,10 +113,20 @@ if (exist ('ssget') ~= 0)
     b = floor (2^20 * b) ;
     fprintf ('.') ;
     x = spex_cholesky_backslash (A, b) ;
+    x1 = spex_ldl_backslash (A, b) ;
     x2 = A\b;
+    err = norm(x-x1)/norm(x);
+    maxerr = max (maxerr, err) ;
     err = norm(x-x2)/norm(x);
     maxerr = max (maxerr, err) ;
+
+    % test a negative definite matrix with LDL
+    x3 = spex_ldl_backslash (-A, b) ;
+    x4 = (-A)\b;
+    err = norm(x3-x4)/norm(x);
+    maxerr = max (maxerr, err) ;
     fprintf ('.') ;
+
 end
 
 orderings = { 'none', 'colamd', 'amd' } ;
@@ -131,29 +142,29 @@ for n = [1 10 100]
         b = rand(n,1);
 
         for korder = 1:length (orderings)
-            %for kpiv = 1:length (pivotings)
-            %    for tol = [0.1 0.5]
 
-                    clear option
-                    option.order = orderings {korder} ;
-%                    option.pivot = pivotings {kpiv} ;
-%                    option.tol   = tol ;
+            clear option
+            option.order = orderings {korder} ;
 
-                    fprintf ('.') ;
-                    x = spex_cholesky_backslash(A,b, option);
-                    x2 = A\b;
-                    err = norm(x-x2)/norm(x);
-                    maxerr = max (maxerr, err) ;
+            fprintf ('.') ;
+            x = spex_cholesky_backslash(A,b, option);
+            x1 = spex_ldl_backslash(A,b, option);
+            x2 = A\b;
+            err = norm(x-x1)/norm(x);
+            maxerr = max (maxerr, err) ;
+            err = norm(x-x2)/norm(x);
+            maxerr = max (maxerr, err) ;
 
-                    % now convert to an integer problem (x will not be integer)
-                    A = floor (2^20 * A) ;
-                    b = floor (2^20 * b) ;
-                    x = spex_cholesky_backslash(A,b, option);
-                    x2 = A\b;
-                    err = norm(x-x2)/norm(x);
-                    maxerr = max (maxerr, err) ;
-                %end
-            %end
+            % now convert to an integer problem (x will not be integer)
+            A = floor (2^20 * A) ;
+            b = floor (2^20 * b) ;
+            x = spex_cholesky_backslash(A,b, option);
+            x1 = spex_ldl_backslash(A,b, option);
+            x2 = A\b;
+            err = norm(x-x1)/norm(x);
+            maxerr = max (maxerr, err) ;
+            err = norm(x-x2)/norm(x);
+            maxerr = max (maxerr, err) ;
         end
     end
 end
@@ -161,9 +172,9 @@ end
 fprintf ('\nmaxerr: %g\n', maxerr) ;
 
 if (maxerr < 1e-6)
-    fprintf('\nSPEX Cholesky installation successful\n')
+    fprintf('\nSPEX Cholesky and LDL tests successful\n')
 else
-    error ('spex_cholesky_backslash:test', '\nTesting failure!  error too high, please reinstall\n')
+    error ('spex_cholesky_backslash:test', '\nTesting failure!\n')
 end
 
 
@@ -178,48 +189,48 @@ for n = [1 10 100]
         b = rand(n,1);
 
         for korder = 1:length (orderings)
-            %for kpiv = 1:length (pivotings)
-            %    for tol = [0.1 0.5]
 
-                    clear option
-                    option.order = orderings {korder} ;
-%                    option.pivot = pivotings {kpiv} ;
-%                    option.tol   = tol ;
+            clear option
+            option.order = orderings {korder} ;
 
-                    fprintf ('.') ;
-                    x = spex_cholesky_backslash(A2,b, option);
-                    x2 = A2\b;
-                    err = norm(x-x2)/norm(x);
-                    maxerr = max (maxerr, err) ;
+            fprintf ('.') ;
+            x = spex_cholesky_backslash(A2,b, option);
+            x1 = spex_ldl_backslash(A2,b, option);
+            x2 = A2\b;
+            err = norm(x-x1)/norm(x);
+            maxerr = max (maxerr, err) ;
+            err = norm(x-x2)/norm(x);
+            maxerr = max (maxerr, err) ;
 
-                    fprintf ('.')
-                    x = spex_lu_backslash(A, b, option);
-                    x2 = A\b;
-                    err = norm(x-x2)/norm(x);
-                    maxerr = max( maxerr, err);
+            fprintf ('.')
+            x = spex_lu_backslash(A, b, option);
+            x2 = A\b;
+            err = norm(x-x2)/norm(x);
+            maxerr = max( maxerr, err);
 
-                    % now convert to an integer problem (x will not be integer)
-                    A2 = floor (2^20 * A2) ;
-                    b = floor (2^20 * b) ;
-                    x = spex_cholesky_backslash(A2,b, option);
-                    x2 = A2\b;
-                    err = norm(x-x2)/norm(x);
-                    maxerr = max (maxerr, err) ;
+            % now convert to an integer problem (x will not be integer)
+            A2 = floor (2^20 * A2) ;
+            b = floor (2^20 * b) ;
+            x = spex_cholesky_backslash(A2,b, option);
+            x1 = spex_ldl_backslash(A2,b, option);
+            x2 = A2\b;
+            err = norm(x-x1)/norm(x);
+            maxerr = max (maxerr, err) ;
+            err = norm(x-x2)/norm(x);
+            maxerr = max (maxerr, err) ;
 
-                    % now convert to an integer problem (x will not be integer)
-                    A = floor (2^20 * A) ;
-                    x = spex_lu_backslash(A,b, option);
-                    x2 = A\b;
-                    err = norm(x-x2)/norm(x);
-                    maxerr = max (maxerr, err) ;
-                %end
-            %end
+            % now convert to an integer problem (x will not be integer)
+            A = floor (2^20 * A) ;
+            x = spex_lu_backslash(A,b, option);
+            x2 = A\b;
+            err = norm(x-x2)/norm(x);
+            maxerr = max (maxerr, err) ;
         end
     end
 end
 
 if (maxerr < 1e-6)
-    fprintf('\nSPEX Backslash installation successful\n')
+    fprintf('\nSPEX Backslash tests\n')
 else
     error ('SPEX_backslash:test', '\nTesting failure!  error too high. Please reinstall\n')
 end
