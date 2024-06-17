27160151c79185f4767462039f29e272b0ea94e4
Tim Davis <davis@tamu.edu>
2024-06-17 16:30:56 -0500

    MATLAB tests: remove ssget

--- SPEX/MATLAB/spex_mex_test.m
+++ SPEX/MATLAB/spex_mex_test.m
@@ -14,32 +14,59 @@ function spex_mex_test
 maxerr = 0 ;
 rng ('default') ;
 
+%-------------------------------------------------------------------------------
+% load in the test matrices
+%-------------------------------------------------------------------------------
+
+try
+    % HB/gr_30_30 matrix (problem ssget (159)):
+    entries = load ('../ExampleMats/gr_30_30.mat.txt') ;
+    gr_30_30 = spconvert (entries (2:end, :)) ;
+    % prob = ssget(159); assert (isequal (gr_30_30, prob.A)) ;
+
+    % HB/494_bus matrix (problem ssget (2)):
+    entries = load ('../ExampleMats/494_bus.mat.txt') ;
+    HB_494_bus = spconvert (entries (2:end, :)) ;
+    % prob = ssget(2) ; assert (isequal (HB_494_bus, prob.A)) ;
+    clear entries
+
+catch me
+    fprintf ('Error: %s\n', me.message) ;
+    fprintf ('This test must be run while the SPEX/MATLAB folder\n') ;
+    fprintf ('current working directory.\n') ;
+    error ('Test matricies not found') ;
+end
+
+%-------------------------------------------------------------------------------
+% Test SPEX Left LU with the HB/gr_30_30 matrix
+%-------------------------------------------------------------------------------
+
 fprintf ('Testing SPEX Left LU: ') ;
 
-% First, check if we can use a real life sparse matrix via ssget
-if (exist ('ssget') ~= 0)
-    fprintf ('. (please wait) ') ;
-    % 159 is a square SPD matrix
-    prob = ssget(159);
-    A = prob.A;
-    [m n] = size(A);
-    b = rand(m, 1);
-    fprintf ('.') ;
-    x = spex_lu_backslash(A,b);
-    x2 = A\b;
-    err = norm(x-x2)/norm(x);
-    maxerr = max (maxerr, err) ;
+fprintf ('. (please wait) ') ;
+A = gr_30_30 ;
 
-    % now convert to an integer problem (x will not be integer)
-    A = floor (2^20 * A) ;
-    b = floor (2^20 * b) ;
-    fprintf ('.') ;
-    x = spex_lu_backslash (A, b) ;
-    x2 = A\b;
-    err = norm(x-x2)/norm(x);
-    maxerr = max (maxerr, err) ;
-    fprintf ('.') ;
-end
+[m n] = size(A);
+b = rand(m, 1);
+fprintf ('.') ;
+x = spex_lu_backslash(A,b);
+x2 = A\b;
+err = norm(x-x2)/norm(x);
+maxerr = max (maxerr, err) ;
+
+% now convert to an integer problem (x will not be integer)
+A = floor (2^20 * A) ;
+b = floor (2^20 * b) ;
+fprintf ('.') ;
+x = spex_lu_backslash (A, b) ;
+x2 = A\b;
+err = norm(x-x2)/norm(x);
+maxerr = max (maxerr, err) ;
+fprintf ('.') ;
+
+%-------------------------------------------------------------------------------
+% Test SPEX LU with random sparse matrices
+%-------------------------------------------------------------------------------
 
 orderings = { 'default', 'none', 'colamd', 'amd' } ;
 pivotings = { 'smallest', 'diagonal', 'first', ...
@@ -89,49 +116,49 @@ else
     error ('\nTesting failure!  error too high\n')
 end
 
+%-------------------------------------------------------------------------------
+% Test SPEX Cholesky and LDL with HB/494_buss
+%-------------------------------------------------------------------------------
+
 fprintf ('Testing SPEX Cholesky and LDL: ') ;
 
-% First, check if we can use a real life sparse matrix via ssget
-if (exist ('ssget') ~= 0)
-    fprintf ('. (please wait) ') ;
-    % 2 is a square SPD matrix
-    prob = ssget(2);
-    A = prob.A;
-    [m n] = size(A);
-    b = rand(m, 1);
-    fprintf ('.') ;
-    x = spex_cholesky_backslash(A,b);
-    x1 = spex_ldl_backslash(A,b);
-    x2 = A\b;
-    err = norm(x-x2)/norm(x);
-    maxerr = max (maxerr, err) ;
-    err = norm(x-x1)/norm(x);
-    maxerr = max (maxerr, err) ;
+% Test with the HB/494_bus matrix:
+A = HB_494_bus ;
+[m n] = size(A);
+b = rand(m, 1);
+fprintf ('.') ;
+x = spex_cholesky_backslash(A,b);
+x1 = spex_ldl_backslash(A,b);
+x2 = A\b;
+err = norm(x-x2)/norm(x);
+maxerr = max (maxerr, err) ;
+err = norm(x-x1)/norm(x);
+maxerr = max (maxerr, err) ;
 
-    % now convert to an integer problem (x will not be integer)
-    A = floor (2^20 * A) ;
-    b = floor (2^20 * b) ;
-    fprintf ('.') ;
-    x = spex_cholesky_backslash (A, b) ;
-    x1 = spex_ldl_backslash (A, b) ;
-    x2 = A\b;
-    err = norm(x-x1)/norm(x);
-    maxerr = max (maxerr, err) ;
-    err = norm(x-x2)/norm(x);
-    maxerr = max (maxerr, err) ;
+% now convert to an integer problem (x will not be integer)
+A = floor (2^20 * A) ;
+b = floor (2^20 * b) ;
+fprintf ('.') ;
+x = spex_cholesky_backslash (A, b) ;
+x1 = spex_ldl_backslash (A, b) ;
+x2 = A\b;
+err = norm(x-x1)/norm(x);
+maxerr = max (maxerr, err) ;
+err = norm(x-x2)/norm(x);
+maxerr = max (maxerr, err) ;
 
-    % test a negative definite matrix with LDL
-    x3 = spex_ldl_backslash (-A, b) ;
-    x4 = (-A)\b;
-    err = norm(x3-x4)/norm(x);
-    maxerr = max (maxerr, err) ;
-    fprintf ('.') ;
+% test a negative definite matrix with LDL
+x3 = spex_ldl_backslash (-A, b) ;
+x4 = (-A)\b;
+err = norm(x3-x4)/norm(x);
+maxerr = max (maxerr, err) ;
+fprintf ('.') ;
 
-end
+%-------------------------------------------------------------------------------
+% Test SPEX Cholesky and LDL with random matrices
+%-------------------------------------------------------------------------------
 
 orderings = { 'none', 'colamd', 'amd' } ;
-%pivotings = { 'smallest', 'diagonal', 'first', ...
-%    'tol smallest', 'tol largest', 'largest' } ;
 
 for n = [1 10 100]
     for density = [0.001 0.05 0.5]
@@ -177,6 +204,9 @@ else
     error ('spex_cholesky_backslash:test', '\nTesting failure!\n')
 end
 
+%-------------------------------------------------------------------------------
+% Test SPEX Backlash with random matrices
+%-------------------------------------------------------------------------------
 
 fprintf ('Testing SPEX Backslash: ') ;
 for n = [1 10 100]
