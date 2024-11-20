1d977e55e27424555d62ad5123272a99b9d2ec01
Tim Davis <DrTimothyAldenDavis@gmail.com>
2024-11-19 21:25:26 -0600

    merge SPEX_Update from Developmental into v3.2.2 to get first cut v3.3.0

--- SPEX/SPEX_Utilities/Source/spex_colamd.c
+++ SPEX/SPEX_Utilities/Source/spex_colamd.c
@@ -77,7 +77,6 @@ SPEX_info spex_colamd
         (double *) NULL, stats);
     if (!colamd_result)
     {
-        printf("fail\n");
         // COLAMD failed: matrix is invalid
         SPEX_FREE_ALL;
         return (SPEX_INCORRECT_INPUT);
